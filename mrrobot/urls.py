09ac2816955b0bc38a43436620970a7dc2f2476a
NickKipshidze <kipshidze.nick@gmail.com>
2024-04-09 10:04:00 +0400

    Removed viewhome path

--- mrrobot/urls.py
+++ mrrobot/urls.py
@@ -7,7 +7,7 @@ from . import views, settings
 urlpatterns = [
     path("robots.txt", lambda request: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
     path("", views.home),
-    path("view", views.viewhome),
+    path("view", views.viewitem),
     path("view/<path:path>", views.viewitem, name="view"),
     path("open/<path:path>", views.openitem, name="open"),
     path("stream/<path:path>", views.stream, name="stream"),
